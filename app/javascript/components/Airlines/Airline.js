148ccb1a9a76e39c4b8c17a2051d7d9ca363eec7
Russell Anderson <russellanderson2000@gmail.com>
2020-07-23 23:54:24 -0400

    using styled-components

---
+++ app/javascript/components/Airlines/Airline.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+`
+const AirlineLogo = styled.div`
+  width: 70px;
+  margin-left: auto;
+  margin-right: auto;
+
+  img {
+    margin-top: 10px;
+    height: 70px;
+    width: 70px;
+    border-radius: 100%;
+    border: 1px solid #efefef;
+  }
+`
+const AirlineName = styled.div`
+  padding: 20px 0 10px 0;
+`
+const LinkWrapper = styled.div`
+  margin: 30px 0 20px 0;
+  height: 50px;
+
+  a {
+      color: #fff;
+      background: #000;
+      border-radius: 4px;
+      padding: 10px 50px;
+      border: 1px solid #000;
+      width: 100%;
+      text-decoration: none;
+  }
+`
+
+
+const Airline = (props) => {
+    return(
+        <Card>
+            <AirlineLogo>
+                <img src={props.attributes.image_url} alt={props.attributes.name}/></AirlineLogo>
+            <AirlineName>{props.attributes.name}</AirlineName>
+            <div className="airline-score">{props.attributes.avg_score}</div>
+            <LinkWrapper>
+                <Link to={'/airlines/${props.attributes.slug}'}>View Airlines</Link>
+            </LinkWrapper>
+        </Card>
+    )
+}
+
+export default Airline
--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,4 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+import Airline from './Airline'
+import styled from 'styled-components'
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  `
+const Heading = styled.div`
+  padding: 100px 100px 10px 100px;
+  
+  h1 {
+      font-size: 40px;
+  }
+  `
+const Subheading = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+`
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
@@ -6,9 +33,31 @@ const Airlines = () => {
   useEffect(()=> {
       //get all airlines from api
       //update airlines in our state
+      axios.get('/api/v1/airlines.json')
+      .then( resp => {
+          setAirlines(resp.data.data)
+      })
+      .catch( resp => console.log(resp) )
+  }, [airlines.length])
+
+  const grid = airlines.map( item => {
+  return (
+    <Airline
+      key={item.attributes.name}
+      attributes={item.attributes}
+    />
+    )
   })
     return(
-    <div>this is the Airlines#index view</div>
+    <Home>
+      <Heading>
+          <h1>Flight Reviews</h1>
+          <Subheading>Unbiased and Honest Airline Reviews</Subheading>
+      </Heading>
+      <Grid>
+        {grid}
+      </Grid>
+    </Home>
     )
 }
 
