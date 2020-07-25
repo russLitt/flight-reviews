791bd1d5d678fa31c9bb8995cd7fd8614bf1f3a7
Russell Anderson <russellanderson2000@gmail.com>
2020-07-24 22:40:47 -0400

    working on build airline show view

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,14 +1,42 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from './Header'
 
 const Airline = (props) => {
     const [airline, setAirline] = useState({})
     const [review, setReview] = useState({})
+    const [loaded, setLoaded] = useState(false)
 
     useEffect(()=> {
-        console.log(props)
+        const slug = props.match.params.slug
+        const url = `/api/v1/airlines/${slug}`
+
+        axios.get(url)
+        .then( resp => {
+            setAirline(resp.data)
+            setLoaded(true)
+        })
+        .catch( resp => console.log(resp) )
     }, [])
 
-    return(<div>this is the Airlines#show view</div>)
+    return(
+    <div className="wrapper">
+        <div className="column">
+            {
+            loaded &&
+            <Header 
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+            }
+            <div className="reviews"></div>
+        </div>
+        <div className="column">
+            <div className="review-form">[Review form goes here]</div>
+        </div>
+
+    </div>
+    )
 }
 
 export default Airline
