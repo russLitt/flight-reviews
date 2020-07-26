2afcd2fe323c5b34564493d3c8d787f6e3138a1e
Russell Anderson <russellanderson2000@gmail.com>
2020-07-25 22:54:43 -0400

    wip: building airline review form

---
+++ app/javascript/components/Airline/ReviewForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ReviewForm = (props) => {
+    return(
+        <div className="wrapper">
+            <form>
+                <div>Share your experience with [airline name] and leave a review</div>
+            </form>
+        </div>
+    )
+}
+
+export default ReviewForm
