74e0b22dd0d71aac5145cd31dda9e1e0be953055
Russell Anderson <russellanderson2000@gmail.com>
2020-08-18 23:50:33 -0400

    star ratings now show on index

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
+import Rating from '../Rating/Rating'
 import styled from 'styled-components'
 
 const Card = styled.div`
@@ -42,7 +43,7 @@ const Airline = (props) => {
             <AirlineLogo>
                 <img src={props.attributes.image_url} alt={props.attributes.name}/></AirlineLogo>
             <AirlineName>{props.attributes.name}</AirlineName>
-            <div className="airline-score">{props.attributes.avg_score}</div>
+            <Rating score={props.attributes.avg_score}/>
             <LinkWrapper>
                 <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link>
             </LinkWrapper>
